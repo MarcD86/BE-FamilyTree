148e7da04fb9b1dd70a1ca32d0521f2df7b6b55b
MarcD86 <marc_duim@live.nl>
2022-10-12 15:26:17 +0200

    secondCommit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Person grootvader = new Person("grootvader", "dijxkie",'m', 67);
+
+
+    }
+
+
+}
